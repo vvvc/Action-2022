a79a601fbafd0b727e5d40de05682fecdebffc54
YAND <vvvc@users.noreply.github.com>
2022-02-04 14:49:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.8.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i 's/10.1.1.5/10.1.1.254/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.99.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/10.1.1.1/10.1.1.100/g' package/base-files/files/bin/config_generate
